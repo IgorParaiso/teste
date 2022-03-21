5aa5ea6b88f2d151614d0f7c2ca4c9f955fba492
Igor_ <Igor_@DESKTOP-GTAS3E6>
2022-03-20 22:36:07 -0300

    Adicionar vendedor

--- ProjetoTesteGit/src/teste2/AppPrincipal.java
+++ ProjetoTesteGit/src/teste2/AppPrincipal.java
@@ -5,6 +5,11 @@ public class AppPrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		Vendedor user = new Vendedor();
+		
+		user.setNome("Igor");
+		System.out.println(user.getNome());
 	}
 
 }
---
+++ ProjetoTesteGit/src/teste2/Vendedor.java
@@ -0,0 +1,15 @@
+package teste2;
+
+public class Vendedor {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
